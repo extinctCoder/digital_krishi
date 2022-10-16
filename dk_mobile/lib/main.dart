0419bc2e4c1fb214f5cdab24d34e3f2755d48182
darthVader <extinctCoder@outlook.com>
2022-10-17 03:39:50 +0600

    update

--- dk_mobile/lib/main.dart
+++ dk_mobile/lib/main.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/material.dart';
-import 'console/chat_home.dart';
-import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+import 'pages/home.dart';
+import 'pages/setting_baudrate.dart';
+import 'pages/setting_device.dart';
+import 'pages/settings.dart';
+import 'stores/app.dart';
+import 'package:provider/provider.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      builder: (context) => AppModel(),
+      child: MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    List<String> availablePorts = SerialPort.availablePorts;
-    print(availablePorts);
-    SerialPort arduino = SerialPort('COM7');
-    arduino.openReadWrite();
-    try {
-      SerialPortReader serial_reader = SerialPortReader(arduino);
-      Stream<String> upcoming_data = serial_reader.stream.map((data) {
-        return String.fromCharCodes(data);
-      });
-      while (true) {
-        upcoming_data.listen((data) {
-          print(data);
-        });
-      }
-    } on SerialPortError catch (err, _) {
-      print(SerialPort.lastError);
-    }
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.lightBlue),
-      home: const SafeArea(child: ChatHome()),
+      title: "Flutter Serial Port Example",
+      routes: {
+        "/setting": (context) => SettingPage(),
+        "/setting/device": (context) => SettingDevicePage(),
+        "/setting/baudrate": (context) => SettingBaudratePage(),
+      },
+      home: HomePage(),
     );
   }
 }
